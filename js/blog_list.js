18675a05d0b9ec5093118eb353c1d1eb1618d863
Gabriel Lima <gabrielslimabusiness@gmail.com>
2020-03-08 20:38:08 -0300

    Add artiles rendering

--- js/blog_list.js
+++ js/blog_list.js
@@ -52,6 +52,7 @@ $(document).ready(function(){
             this.articles_info.forEach(article => {
                 console.log(article);
                 enriched_article = article_template.replace('{{post_id}}', article.id);
+                console.log(enriched_article);
                 enriched_article = article_template.replace('{{post.title}}', article.title);
                 enriched_article = article_template.replace('{{post.author}}', article.author);
                 enriched_article = article_template.replace('{{post.date}}', article.date);
